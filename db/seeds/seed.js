6d320015da7aeea80df62922420a72fe18f6341e
manntom37 <thomasemann4@gmail.com>
2022-01-17 14:14:26 +0000

    added CREATE TABLES for all

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,7 +1,35 @@
+const db = require("../connection.js");
+const format = require("pg-format");
+
 const seed = (data) => {
   const { articleData, commentData, topicData, userData } = data;
   // 1. create tables
-  // 2. insert data
+  return db
+    .query(`DROP TABLE IF EXISTS comments, articles, topics, users;`)
+    .then(() => {
+      return db.query(`CREATE TABLE users (
+        username TEXT PRIMARY KEY,
+        avatar_url TEXT,
+        name TEXT
+      )`);
+    })
+    .then(() => {
+      return db.query(`CREATE TABLE topics (
+      slug TEXT PRIMARY KEY,
+      description TEXT NOT NULL 
+    )`);
+    })
+    .then(() => {
+      return db.query(`CREATE TABLE articles (
+        article_id SERIAL PRIMARY KEY,
+        title TEXT,
+        body TEXT NOT NULL,
+        votes INT,
+        topic TEXT REFERENCES topics(slug),
+        author TEXT REFERENCES users(username),
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      )`);
+    })
 };
 
 module.exports = seed;
