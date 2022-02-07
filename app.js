aa8d37449d45a6a0d14d041f486cecd264d6dd64
manntom37 <thomasemann4@gmail.com>
2022-02-07 12:26:45 +0000

    express fix

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const cors = require('cors');
+const express = require("express");
 const app = express();
 app.use(cors());
 
-const express = require("express");
 const {
   getArticlesById,
 } = require("./controllers/getArticlesById.controllers");
